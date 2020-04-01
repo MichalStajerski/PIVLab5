f6d19636e79721a7143eb1c64b522ad4d316d19a
MichalStajerski <m918560@gmail.com>
2020-04-01 23:17:46 +0200

    dodano Lab5

---
+++ Lab5_TSES/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_TSES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var es = new ESContext();
+            es.Computers.Add(new ESComputer() { Description = "ESComputer", CoolingType = "?", Price = 1500 });
+            es.SaveChanges();
+
+
+            var ts = new TSContext();
+            ts.Computers.Add(new TSComputer()
+            {
+                Description = "ABC",
+                CoolingType = "Hybrid",
+                Price = 666,
+                Server = new Server() { Bandwidth = 1300 }
+            });
+            ts.SaveChanges();
+
+
+        }
+    }
+}
